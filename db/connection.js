0308a05f5aa1d998b72778a455f049224b4289a9
Marvin Obig <88102362+marvinobig@users.noreply.github.com>
2022-08-05 14:02:09 +0100

    setup production environment

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,24 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
-const db = new Pool();
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
+
+const db = new Pool(config);
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error("PGDATABASE not set");
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABSE_URL not set");
 }
 
 module.exports = db;
